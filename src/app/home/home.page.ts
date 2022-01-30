9e660de65242716fd1e808944da2de589b06e628
Erik Martinez <emrbeltane@gmail.com>
2022-01-30 20:56:19 +0100

    Select players

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  players = [];
+  all_players = ["Erik", "Martin"]
 
   constructor() {}
-
 }
